3f1fdbeb72e27bad696ec78811025c04244e55f7
odelfyette <odelfyette@gmail.com>
2018-03-01 15:32:27 -0500

    set up node server to send form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -11,6 +14,24 @@ export class HomeComponent {
   model = new Employee('Darla', 'Smith', true, 'w2', "default");
   hasPrimaryLanguageError = false;
 
+  constructor(private formPoster: FormPoster){
+
+  }
+  
+  submitForm(form: NgForm){
+    //validate form
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    
+    if(this.hasPrimaryLanguageError)
+      return;
+
+    this.formPoster.postEmployeeForm(this.model)
+        .subscribe(
+          data => console.log('success: ', data),
+          err => console.log('error: ', err)
+        );
+  }
+
   firstNameToUpperCase(value: string){
     if(value.length > 0)
       this.model.firstName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
@@ -19,6 +40,6 @@ export class HomeComponent {
   }
 
   validatePrimaryLanguage(value){
-    this.hasPrimaryLanguageError = (value !== 'default');
+    this.hasPrimaryLanguageError = (value === 'default');
   }
 }
